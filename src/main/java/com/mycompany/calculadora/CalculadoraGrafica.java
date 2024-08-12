6ac4242ee2f5df4c5bb169b102091af14c38bb4f
sergio <est.sergioa.contre1@unimilitar.edu.co>
2024-08-12 15:07:29 -0500

    Add files via upload
    creación y posicionamiento de botones en pantalla, creación de acciones para los botones (generan texto en el JTextField)

--- src/main/java/com/mycompany/calculadora/CalculadoraGrafica.java
+++ src/main/java/com/mycompany/calculadora/CalculadoraGrafica.java
@@ -1,7 +1,10 @@
 package com.mycompany.calculadora;
 
 
+import java.awt.Color;
 import java.awt.TextField;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -20,7 +23,8 @@ public class CalculadoraGrafica {
         Ventana = new JFrame("Calculadora Grafica");
         Panel = new JPanel();
         Panel.setLayout(null);
-        Panel.setBounds(0, 0, 300, 400); 
+        Panel.setBounds(0, 0, 300, 400);
+        //Panel.setBackground(Color.yellow);
         Ventana.setLayout(null);
         Ventana.add(Panel);
         Ventana.setLocationRelativeTo(null);
@@ -35,26 +39,119 @@ public class CalculadoraGrafica {
     }
      
     private void Buttons(){
-        b1=new JButton("1"); b1.setBounds(10,70,35, 35);      
-        b2=new JButton("2"); b2.setBounds(10,110,35, 35);
-        b3=new JButton("3"); b3.setBounds(10,150,35, 35);
-        b4=new JButton("4"); b4.setBounds(50,70,35, 35);
-        b5=new JButton("5"); b5.setBounds(50,110,35, 35);
-        b6=new JButton("6"); b6.setBounds(50,150,35, 35);
-        b7=new JButton("7"); b7.setBounds(90,70,35, 35);
-        b8=new JButton("8"); b8.setBounds(90,110,35, 35);
-        b9=new JButton("9"); b9.setBounds(90,150,35, 35);
-        b0=new JButton("0"); b0.setBounds(130,0,35, 70);
-        //organizar los botones en la pantalla
-        Panel.add(b1);
-        Panel.add(b2);
-        Panel.add(b3);
-        Panel.add(b4);
-        Panel.add(b5);
-        Panel.add(b6);
-        Panel.add(b7);
-        Panel.add(b8);
-        Panel.add(b9);
-        Panel.add(b0);
+        int DistanceButtonsy= 50+5;//se multiplica por la posicion del boton contando desde 0
+        int DistanceButtonsx=50+5;//se multiplica por la posicion del boton contando desde 0
+        b1=new JButton("1"); b1.setBounds(10,70+DistanceButtonsy*3,50, 50);                  Panel.add(b1);   
+        b2=new JButton("2"); b2.setBounds(10+DistanceButtonsx,70+DistanceButtonsy*3,50, 50);   Panel.add(b2);
+        b3=new JButton("3"); b3.setBounds(10+DistanceButtonsx*2,70+DistanceButtonsy*3,50, 50); Panel.add(b3);
+        b4=new JButton("4"); b4.setBounds(10,70+DistanceButtonsy*2,50, 50);                  Panel.add(b4);
+        b5=new JButton("5"); b5.setBounds(10+DistanceButtonsx,70+DistanceButtonsy*2,50, 50);   Panel.add(b5);
+        b6=new JButton("6"); b6.setBounds(10+DistanceButtonsx*2,70+DistanceButtonsy*2,50, 50); Panel.add(b6);
+        b7=new JButton("7"); b7.setBounds(10,70+DistanceButtonsy,50, 50);                  Panel.add(b7);
+        b8=new JButton("8"); b8.setBounds(10+DistanceButtonsx,70+DistanceButtonsy,50, 50);   Panel.add(b8);
+        b9=new JButton("9"); b9.setBounds(10+DistanceButtonsx*2,70+DistanceButtonsy,50, 50); Panel.add(b9);
+        b0=new JButton("0"); b0.setBounds(10+DistanceButtonsx,70+DistanceButtonsy*4,50, 50);  Panel.add(b0);  
+        /*BOTONES DE OPERACIONES*/
+        JButton Buttonsum= new JButton("+"); Buttonsum.setBounds(10+DistanceButtonsx*3, 70+DistanceButtonsy*3, 50, 50);Panel.add(Buttonsum);
+        JButton Buttonrest= new JButton("-");Buttonrest.setBounds(10+DistanceButtonsx*3, 70+DistanceButtonsy*2, 50, 50); Panel.add(Buttonrest);
+        JButton Buttonmult= new JButton("*");Buttonmult.setBounds(10+DistanceButtonsx*3, 70+DistanceButtonsy, 50, 50); Panel.add(Buttonmult);
+        JButton Buttondiv= new JButton("/");Buttondiv.setBounds(10+DistanceButtonsx*3, 70, 50, 50); Panel.add(Buttondiv);
+        JButton Buttondot= new JButton("."); Buttondot.setBounds(10+DistanceButtonsx*2, 70+DistanceButtonsy*4, 50, 50); Panel.add(Buttondot);
+        JButton Buttonequal= new JButton("="); Buttonequal.setBounds(10+DistanceButtonsx*3, 70+DistanceButtonsy*4, 50, 50); Panel.add(Buttonequal);
+        /*-----funciones EXTRAS-----*/
+        
+        
+        
+        
+        
+        /*-----ACCIONES DE LOS BOTONES-----*/
+        /*-----ACCIONES BUTTONS NUMERICOS------*/
+        b1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+               TxtPantalla.setText(TxtPantalla.getText() + "1");                
+            }
+        });
+         //b2.addActionListener(new ActionListener())
+        b2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+            TxtPantalla.setText(TxtPantalla.getText() + "2");                
+            }
+        });
+        b3.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+            TxtPantalla.setText(TxtPantalla.getText() + "3");    
+            }
+        });
+        b4.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+            TxtPantalla.setText(TxtPantalla.getText() + "4");     
+            }
+        });
+        b5.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+             TxtPantalla.setText(TxtPantalla.getText() + "5");      
+            }
+        });
+        b6.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+             TxtPantalla.setText(TxtPantalla.getText() + "6");     
+            }
+        });
+        b7.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+             TxtPantalla.setText(TxtPantalla.getText() + "7");     
+            }
+        });
+        b8.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+             TxtPantalla.setText(TxtPantalla.getText() + "8");     
+            }
+        });
+        b9.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+              TxtPantalla.setText(TxtPantalla.getText() + "9");        
+            }
+        });
+        b0.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+            TxtPantalla.setText(TxtPantalla.getText() + "0");     
+            }
+        });
+        /*-----ACCIONES BUTTONS OPERACIONES-----*/
+        Buttonsum.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+            TxtPantalla.setText(TxtPantalla.getText() + "+");     
+            }
+        });
+        Buttonrest.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+             TxtPantalla.setText(TxtPantalla.getText() + "-");     
+            }
+        });
+        Buttonmult.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+            TxtPantalla.setText(TxtPantalla.getText() + "*");     
+            }
+        });
+        Buttondiv.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+            TxtPantalla.setText(TxtPantalla.getText() + "/");     
+            }
+        });
     }
 }
+ 
